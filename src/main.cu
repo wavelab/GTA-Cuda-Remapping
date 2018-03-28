ef9c69377e93f15481a4fdb2317eefa86c0a6354
Matt Angus <m2angus@wisebob.eng.uwaterloo.ca>
2018-03-28 19:57:14 -0400

    improved parallelism

--- src/main.cu
+++ src/main.cu
@@ -7,6 +7,7 @@
 #include <chrono>
 #include <regex>
 #include <experimental/filesystem>
+#include <cuda_runtime_api.h>
 #include <opencv2/opencv.hpp>
 #include <thread>
 #include <mutex>
@@ -33,9 +34,21 @@ namespace fs = std::experimental::filesystem;
 // 	done = true;
 // }
 
-void processFiles(std::vector<std::string> files, std::string outfile)
+const int device = 2;
+
+bool hasEnding (std::string const &fullString, std::string const &ending) {
+    if (fullString.length() >= ending.length()) {
+        return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
+    } else {
+        return false;
+    }
+}
+
+void processFiles(std::vector<std::string> files, std::string image_path, std::string output_path, std::string outfile)
 {
-	std::ofstream fout(outfile);
+	cudaSetDevice(device);
+	auto& fout = std::cout;
+	//std::ofstream fout(outfile, std::ofstream::out | std::ofstream::app);
 	std::unique_ptr<GpuMat<unsigned char>> scratchGpuMat;
 	std::unique_ptr<GpuMat<unsigned char>> outputImg;
 
@@ -46,7 +59,20 @@ void processFiles(std::vector<std::string> files, std::string outfile)
 	{
 		auto readStart = std::chrono::high_resolution_clock::now();
 		fout << "reading " << files[i] << std::endl;
-		cv::Mat img = cv::imread(files[i], CV_LOAD_IMAGE_COLOR);
+		cv::Mat img;
+		while(!img.data)
+		{
+			try
+			{
+				img = cv::imread(files[i], CV_LOAD_IMAGE_COLOR);
+			}
+			catch(std::exception& ex)
+			{
+				fout << "caught a thing" << std::endl;
+			}
+			if(!img.data) //only be able to parse if IEND chunk is found (i.e. transer complete)
+				std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
 		auto readEnd = std::chrono::high_resolution_clock::now();
 
 		if(i == 0)
@@ -59,7 +85,7 @@ void processFiles(std::vector<std::string> files, std::string outfile)
 		
 		cv::Mat outMat = outputImg->getMat();
 
-		std::string outFileName = std::regex_replace(files[i], std::regex("n_"), "r_");
+		std::string outFileName = std::regex_replace(files[i], std::regex(image_path), output_path);
 		fs::create_directories(fs::path(outFileName).parent_path());
 		fout << "writing " << outFileName << std::endl;
 		cv::imwrite(outFileName, outMat);
@@ -98,6 +124,26 @@ std::vector<std::vector<T>> SplitVector(const std::vector<T>& vec, size_t n)
     return outVec;
 }
 
+std::vector<std::string> GetImagesToProcess(std::string& inputPath, std::string& outputPath)
+{
+	std::vector<std::string> ret;
+	for(auto& p: fs::recursive_directory_iterator(inputPath))
+	{
+		std::string curPath = p.path().string();
+		bool regFile = fs::is_regular_file(p);
+		if(regFile && hasEnding(curPath, "png"))
+		{
+			std::string imgPath = std::regex_replace(curPath, std::regex(inputPath), outputPath);
+
+			if(!fs::is_regular_file(imgPath))
+			{
+				ret.push_back(curPath);
+			}
+		}
+	}
+	return ret;
+}
+
 /**
  * contains cuda specific initializations
  */
@@ -105,7 +151,6 @@ int main(int argc, char** argv )
 {	
 	// grab the arguments
 	std::string image_path;
-	std::string coloursFile;
 	std::string output_path;
 	int numProc = 8;
 	std::vector<int> availGpu = {0,1,2};
@@ -115,39 +160,48 @@ int main(int argc, char** argv )
 			image_path = argv[i+1];
 		if (strcmp(argv[i], "-o") == 0)
 			output_path = argv[i+1];
-		if (strcmp(argv[i], "-c") == 0)
-			coloursFile = argv[i+1];
 		if (strcmp(argv[i], "-n") == 0)
 			numProc = atoi(argv[i+1]);
 	}
 
-	std::vector<std::string> all_files;
+	// std::vector<std::string> all_files;
 
-	for(auto& p: fs::recursive_directory_iterator(image_path))
+	// for(auto& p: fs::recursive_directory_iterator(image_path))
+	// {
+	// 	std::string val = p.path().string();
+	// 	std::string fname = p.path().filename().string();
+	// 	if(Helpers::hasEnding(val,"png") && fname[0] == 'n')
+	// 		all_files.push_back(val);
+	// }
+
+	// std::cout << "found " << all_files.size() << " images" << std::endl;
+
+	// std::vector<std::vector<std::string>> chunks = SplitVector(all_files, numProc);
+	cudaSetDevice(device);
+
+	for(;;)
 	{
-		std::string val = p.path().string();
-		std::string fname = p.path().filename().string();
-		if(Helpers::hasEnding(val,"png") && fname[0] == 'n')
-			all_files.push_back(val);
+		std::vector<std::string> toProcess = GetImagesToProcess(image_path, output_path);
+		if(toProcess.size() > 0)
+		{
+			std::vector<std::vector<std::string>> chunks = SplitVector(toProcess, numProc);
+
+			std::vector<std::thread> threads;
+
+			for(int i = 0; i < numProc && i < chunks.size(); i++)
+			{
+				std::stringstream ss;
+				ss << i << ".out";
+				threads.push_back(std::thread(processFiles, chunks[i], image_path, output_path, ss.str()));
+			}
+
+			for(std::thread& t : threads)
+			{
+				t.join();
+			}
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 	}
 
-	std::cout << "found " << all_files.size() << " images" << std::endl;
-
-	std::vector<std::vector<std::string>> chunks = SplitVector(all_files, numProc);
-
-	std::vector<std::thread> threads;
-
-	for(int i = 0; i < numProc; i++)
-	{
-		std::stringstream ss;
-		ss << i << ".out";
-		threads.push_back(std::thread(processFiles, chunks[i], ss.str()));
-	}
-
-	for(int i = 0; i < numProc; i++)
-	{
-		threads[i].join();
-	}
-
-	return 0;
+	//return 0;
 }
